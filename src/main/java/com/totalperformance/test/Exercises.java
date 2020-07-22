56f924b3778861feae53d8e82cb883ee76d0ceb2
Eric de la Cruz <ericmdelacruz@gmail.com>
2020-07-22 17:44:12 -0300

    Initial project structure

--- src/main/java/com/totalperformance/test/Exercises.java
+++ src/main/java/com/totalperformance/test/Exercises.java
@@ -12,7 +12,7 @@ public class Exercises {
 
     private Dictionary elementDictionaty = new Dictionary() {
         @Override
-        public int size() {
+        public int size( ) {
             return 0;
         }
 
